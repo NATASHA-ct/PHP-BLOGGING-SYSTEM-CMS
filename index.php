2294785545d94030e9d61186eebd6fdec9851dbe
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-03-10 18:16:34 +0200

    Fetch and dispay post

--- index.php
+++ index.php
@@ -11,10 +11,49 @@ include "includes/db.php";
     <title>Document</title>
 </head>
 <body>
-    <div>
+   
 <p>HELLO</p>
 
+<div>
 
+<?php
+
+ $sql = "SELECT * FROM `posts`";
+ $select_all_posts = mysqli_query($connect, $sql);
+
+    while($row = mysqli_fetch_assoc($select_all_posts)){
+        $post_title = $row['post_title'];
+        $post_author = $row['post_author'];
+        $post_date = $row['post_date'];
+        $post_image = $row['post_image'];
+        $post_content = $row['post_content'];
+    
+        echo "<h2><a href='#'>{$post_title}</a></h2>";
+
+        echo "<p class='lead'>by <a href='index.php'>{$post_author}</a></p>";
+
+        echo "<p><span class='glyphicon glyphicon-time'></span> Posted on {$post_date}</p>";
+
+
+        echo "<hr>";
+
+        echo "<img class='img-responsive' src='images/{$post_image}' alt=''>";
+
+        echo "<hr>";
+
+        echo "<p>{$post_content}</p>";
+
+        echo "<a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a>";
+
+        echo "<hr>";
+    }
+
+
+?>
+  <p></p>
+
+</div>
+ <div>
     <?php 
     $sql = "SELECT * FROM `categories`";
 
